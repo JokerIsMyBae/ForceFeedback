13d4073f2fa28639f86bb91557eeb82565e52dee
V-donck <tv26363@gmail.com>
2021-03-20 18:34:21 +0100

    run motor 1 second in 2 directions

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,30 @@
 #include <Arduino.h>
 
-#define ENA 2
+#define EnA 2
 #define In1 3
 #define In2 4
 
+int motorspeed = 0;
 
 void setup() {
-  pinMode(ENA, OUTPUT);
+  pinMode(EnA, OUTPUT);
   pinMode(In1, OUTPUT);
   pinMode(In2, OUTPUT);
+  motorspeed = 150;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  // turn 1 way
+  digitalWrite(In1, LOW);
+  digitalWrite(In2, HIGH);
+  analogWrite(EnA, motorspeed); // turn motor 1 way
+  delay(1000);
+  analogWrite(EnA, 0); // turn of motor
+  // turn other way
+  digitalWrite(In1, HIGH);
+  digitalWrite(In2, LOW);
+  analogWrite(EnA, motorspeed); // turn motor other way
+  delay(1000);
+  analogWrite(EnA, 0); // turn of motor
 }
