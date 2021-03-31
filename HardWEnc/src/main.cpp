355ea60b4852ae7366899d275b33d25154e34b57
Joos Van Esbroeck <58085617+JokerIsMyBae@users.noreply.github.com>
2021-03-31 15:59:52 +0200

    Hardware versie

---
+++ HardWEnc/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <Encoder.h>
+
+// Change these pin numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder myEnc(0, 1);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+long position  = 9999;
+
+void loop() {
+  long newLeft;
+  newLeft = myEnc.read();
+  if (newLeft != position) {
+    Serial.print("Position = ");
+    Serial.print(newLeft);
+    Serial.println();
+    position = newLeft;
+  }
+}
